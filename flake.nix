e6d61f0941c1be773c89c66e6b4c5e407922df8c
kamil <frankiewiczkamil@gmail.com>
2026-02-08 13:27:14 +0100

    Initialize local env - `flake` and `direnv` :wrench: :hammer:

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Nix App Version Inspector";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      lib = nixpkgs.lib;
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gopls
+              delve
+            ];
+          };
+        }
+      );
+    };
+}
