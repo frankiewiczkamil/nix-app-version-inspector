9126413b8c37e3eb4479ceb99d204ee3a79e5957
kamil <frankiewiczkamil@gmail.com>
2026-02-19 00:07:07 +0100

    Replace querying using `nix-store --gc` with pure `ls` :zap:

--- main.go
+++ main.go
@@ -8,23 +8,30 @@ import (
 	"strings"
 )
 
+var NIX_STORE_DEFAULT_PATH = "/nix/store"
+
 func findActiveVersions(queriedName string) bytes.Buffer {
 	// consider filtering out derivations (| grep -v '\.drv$')
-	// consider using pure ls for performance reasons
 
-	cmd1 := exec.Command("nix-store", "--gc", "--print-live")
+	cmd1 := exec.Command("ls", NIX_STORE_DEFAULT_PATH, "-1")
 	cmd2 := exec.Command("grep", queriedName)
+	cmd3 := exec.Command("sed", "s|^|"+NIX_STORE_DEFAULT_PATH+"/|")
 
-	pipe, _ := cmd1.StdoutPipe()
-	cmd2.Stdin = pipe
+	pipe1, _ := cmd1.StdoutPipe()
+	cmd2.Stdin = pipe1
+
+	pipe2, _ := cmd2.StdoutPipe()
+	cmd3.Stdin = pipe2
 
 	var output bytes.Buffer
-	cmd2.Stdout = &output
+	cmd3.Stdout = &output
 
 	cmd1.Start()
 	cmd2.Start()
+	cmd3.Start()
 	cmd1.Wait()
 	cmd2.Wait()
+	cmd3.Wait()
 
 	return output
 }
